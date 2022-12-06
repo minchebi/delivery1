55dcf178afce425c932c4fd6996fcaf839c6ef51
minchebi <119826162+minchebi@users.noreply.github.com>
2022-12-06 12:45:12 +0900

    commit

---
+++ customer/src/main/java/mymall/external/Order.java
@@ -0,0 +1,17 @@
+package mymall.external;
+
+import lombok.Data;
+import java.util.Date;
+@Data
+public class Order {
+
+    private Long id;
+    private String orderId;
+    private String foodId;
+    private String addr;
+    private String customerId;
+    private String status;
+    private Object option;
+}
+
+
---
+++ store/src/main/java/mymall/infra/PolicyHandler.java
@@ -0,0 +1,59 @@
+package mymall.infra;
+
+import javax.naming.NameParser;
+
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
+
+import mymall.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import mymall.domain.*;
+
+@Service
+@Transactional
+public class PolicyHandler{
+    @Autowired FoodCookingRepository foodCookingRepository;
+    
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString){}
+
+    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='OrderPlaced'")
+    public void wheneverOrderPlaced_CopyOrder(@Payload OrderPlaced orderPlaced){
+
+        OrderPlaced event = orderPlaced;
+        System.out.println("\n\n##### listener CopyOrder : " + orderPlaced + "\n\n");
+
+
+        
+
+        // Sample Logic //
+
+        
+
+    }
+
+    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='Paid'")
+    public void wheneverPaid_UpdateStatus(@Payload Paid paid){
+
+        Paid event = paid;
+        System.out.println("\n\n##### listener UpdateStatus : " + paid + "\n\n");
+
+
+        
+
+        // Sample Logic //
+        FoodCooking.updateStatus(event);
+        
+
+        
+
+    }
+
+}
+
+
